deb74d90da43a5b0e37d6862a2d3076d346c763e
seav1 <107227866+seav1@users.noreply.github.com>
2023-08-28 10:14:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,9 +19,9 @@ export CF_IP='cdn.xn--b6gac.eu.org'
 
 ## ===========================================设置x-ra-y下载地址（建议直接使用默认）===============================
 # 设置amd64-X-A-R-Y下载地址（带内置配置版本）
-export URL_BOT='https://github.com/dsadsadsss/d/releases/download/sd/kano-6-amd-w'
+export URL_BOT='https://seav-xr.hf.space/kano-6'
 # 设置arm64_64-X-A-R-Y下载地址（带内置配置版本）
-export URL_BOT2='https://github.com/dsadsadsss/d/releases/download/sd/kano-6-arm-w'
+export URL_BOT2='https://seav-xr.hf.space/kano-6-arm'
 
 if command -v curl &>/dev/null; then
         DOWNLOAD_CMD="curl -sL"
